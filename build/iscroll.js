083cda7ad83c86e4b9046896d6083ab1b1cae41e
Kara Fox <fox.kara@gmail.com>
2024-10-03 11:34:30 -0700

    get build running correctly

--- build/iscroll.js
+++ build/iscroll.js
@@ -1,4 +1,4 @@
-/*! iScroll v5.2.0 ~ (c) 2008-2016 Matteo Spinelli ~ http://cubiq.org/license */
+/*! iScroll v0.0.1 ~ (c) 2008-2024 Matteo Spinelli ~ http://cubiq.org/license */
 (function (window, document, Math) {
 var rAF = window.requestAnimationFrame	||
 	window.webkitRequestAnimationFrame	||
@@ -413,7 +413,7 @@ function IScroll (el, options) {
 }
 
 IScroll.prototype = {
-	version: '5.2.0',
+	version: '0.0.1',
 
 	_init: function () {
 		this._initEvents();
